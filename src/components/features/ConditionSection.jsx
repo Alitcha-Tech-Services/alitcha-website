b438d6ce54af44fa71018a6946cce82dd5526765
Techteamblt <kinninkpomathias@gmail.com>
2024-05-06 14:12:11 +0100

    creation des composants merge main dev

---
+++ src/components/features/ConditionSection.jsx
@@ -0,0 +1,36 @@
+import '../../styles/features/condition.css'
+
+export default function ConditionSection () {
+
+    return (
+        <>
+            <div className='w-75 m-auto'>
+                <h3 className='mb-5'>Conditions</h3>
+                <div className='condition-container w-100 m-auto'>
+                    <div className='condition-next-container h-100 d-flex align-items-center'>
+                        <div className='h-75 w-25 d-flex flex-column justify-content-around align-items-center ms-5' style={{fontSize:'20px'}}>
+                            <h5>Pour rejoindre Alitcha, il faut :</h5>
+                            <span className='text-color-primary'>En tant que membre</span>
+                            <p>
+                                01 : Jorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            <p>
+                                01 : Jorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            <p>
+                                01 : Jorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            <p>
+                                01 : Jorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+    )
+}
---
+++ src/components/features/ResearchProject.jsx
@@ -0,0 +1,23 @@
+
+import '../../styles/features/researchProjectSection.css'
+import PaginateIcons from '../commons/PaginateIcons'
+export default function ResearchProject(){
+
+    return (
+        <>
+            <div id="research-project-section" className="d-flex flex-column m-auto my-5">
+                <h5 className="my-3">
+                    Les Projets de Recherche
+                </h5>
+                <div className="text-justify my-3">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure quo recusandae debitis cum, sint nisi laboriosam veritatis ut aspernatur porro facilis dolorem pariatur non vitae fugit incidunt minima consequatur minus.
+                    Ea voluptas dolorem architecto blanditiis, nulla consequatur facilis dolores quo fuga tempore cupiditate. Ipsum repellendus quos doloribus id eius est quisquam ullam alias nobis error beatae omnis, veritatis quod nam.
+                    Repellat fuga perspiciatis similique blanditiis esse possimus accusamus placeat vitae numquam doloremque necessitatibus delectus porro officiis assumenda alias dignissimos libero quia consequatur odit beatae laborum est, nulla quis reprehenderit. Similique?
+                    <div className='d-flex justify-content-center align-items-center flex-row'>
+                        <PaginateIcons />   
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
