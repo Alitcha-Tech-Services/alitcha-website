4e12d7c0c6cd5d3fd8685cabda3e79ae8e0bec03
TCHONKLOE Thierry <thierry@alitchateam.com>
2024-05-10 18:33:52 +0100

    mettre le code propre après avoir mergé

--- src/pages/Events/Event.jsx
+++ src/pages/Events/Event.jsx
@@ -1,11 +1,9 @@
 import './Event.css';
-import Header from '../../components/features/Header'
 import ConferenceSection from '../../components/features/ConferenceSection';
 import AtelierSection from '../../components/features/AtelierSecion';
 import ProgrammeSection from '../../components/features/ProgrammeSection';
 import FormationSection from '../../components/features/FormationSection';
 import Newsletter from '../../components/features/Newsletter';
-import Footer from '../../components/features/Footer';
 import RejoindreAlitchaSection from '../../components/commons/RejoindreAlitchaSection';
 import HeroHeader from '../../components/commons/HeroHeader';
 
@@ -13,7 +11,6 @@ export default function Event()
 {
     return(
         <>
-        <Header />
         <HeroHeader />
         <ConferenceSection />
         <AtelierSection />
@@ -21,8 +18,6 @@ export default function Event()
         <FormationSection />
         <RejoindreAlitchaSection />
         <Newsletter />
-        <Footer />
-
         </>
     )
 }
